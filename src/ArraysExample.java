9d6b01c15d73105ba7029003b326356d789e5bf3
Simon <sweetsixteen.sudo@gmail.com>
2020-03-06 13:59:03 +0300

    Everything I need is commit

--- src/ArraysExample.java
+++ src/ArraysExample.java
@@ -10,40 +10,58 @@ public class ArraysExample {
         if (s == null) {
             System.out.println("true");
         }
+
         int n = 10;
         arr = getRandomArray(n);
-        for (int x : arr) {
-            System.out.println(x);
+        for (int javaLoveDi : arr) {
+            System.out.println(javaLoveDi);
+            javaLoveDi = 20;
         }
-        System.out.println(arr.toString());
         System.out.println(Arrays.toString(arr));
+        System.out.println(arr.toString());
+
+
+
         int b[] = new int[n];
         for (int i = 0; i < n; i++) {
             b[i] = arr[i];
         }
-
-        for (int x : b) {
-            System.out.println(x);
-        }
         System.out.println("__________________________________________");
-        arr[5] = 34214;
-        for (int x : b) {
-            System.out.println(x);
-        }
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
+        boolean result = true;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == b[i]){
+                continue;
+            }
+            if(arr[i] != b[i]){
+                result = false;
+                break;
+            }
+        }
+
         boolean b1 = Arrays.equals(arr,b);
         System.out.println(b1);
         int [] a1 = new int[5];
         int [] a2 = {3,5,6,7,2,4,7,3,1};
+        char[] arr1 = getRandomCharArray(n);
+        System.out.println(Arrays.toString(arr1));
     }
 
     public static int[] getRandomArray(int n) {
-        int[] help = new int[n];
+        int[] array = new int[n];
         Random random = new Random();
         for (int i = 0; i < n; i++) {
-            help[i] = random.nextInt();
+            array[i] = random.nextInt();
         }
-        return help;
+        return array;
+    }
+    public static char[] getRandomCharArray(int n){
+        char[] newArray = new char[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            newArray[i] = (char) random.nextInt();
+        }
+        return newArray;
     }
 }
